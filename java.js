737539a9032e0dbebb6b8d78cc4e7b3cdc5f2bee
Robert Bovee <Rjbovee112@gmail.com>
2019-12-31 19:05:39 -0700

    form java finished?

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 const includedUppercaseElement = document.getElementById('includedUppercase')
 const includedLowercaseElement = document.getElementById('includedLowercase')
-const includedUNumericElement = document.getElementById('includedUNumeric')
+const includedNumericElement = document.getElementById('includedNumeric')
 const includedSpecialElement = document.getElementById('includedSpecial')
 const characterNumber = document.getElementById('characterNumber')
 const form = document.getElementById('passwordGen')
@@ -10,5 +10,9 @@ const form = document.getElementById('passwordGen')
 form.addEventListener('submit', e => {
     e.preventDefault()
     const characterNumber = characterNumber.value
+    const includedUppercase = includedUppercaseElement.checked
+    const includedLowercase = includedLowercaseElement.checked
+    const includedNumeric = includedNumericElement.checked
+    const includedSpecial = includedSpecialElement.checked
     const pasword = genPassword(characterNumber, includedUppercase, includedLowercase, includedNumeric, includedSpecial)
 })

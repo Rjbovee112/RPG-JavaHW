c84baa86a2e4d32bef76d631863aa496ec2c00f1
Robert Bovee <Rjbovee112@gmail.com>
2019-12-31 18:44:17 -0700

    added const for each id checkbox

--- java.js
+++ java.js
@@ -1,6 +1,14 @@
+const includedUppercaseElement = document.getElementById('includedUppercase')
+const includedLowercaseElement = document.getElementById('includedLowercase')
+const includedUNumericElement = document.getElementById('includedUNumeric')
+const includedSpecialElement = document.getElementById('includedSpecial')
+const characterNumber = document.getElementById('characterNumber')
 const form = document.getElementById('passwordGen')
+
+
 
 form.addEventListener('submit', e => {
     e.preventDefault()
+    const characterNumber = characterNumber.value
     const pasword = genPassword(characterNumber, includedUppercase, includedLowercase, includedNumeric, includedSpecial)
 })
